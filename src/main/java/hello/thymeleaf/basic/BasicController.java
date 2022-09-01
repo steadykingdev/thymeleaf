6a2c3fe82d8fd7f8f26f963f8c58d8541b77e7d1
steadykingdev <steadykingdev@gmail.com>
2022-09-01 21:48:40 +0900

    literal

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -81,6 +81,12 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "spring!");
+        return "basic/literal";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
