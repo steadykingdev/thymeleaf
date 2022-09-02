6eb335f4a467b45179ba36498ce0274fb7db2065
steadykingdev <steadykingdev@gmail.com>
2022-09-02 12:30:21 +0900

    속성 값 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -87,6 +87,12 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+
     @GetMapping("/operation")
     public String operation(Model model) {
         model.addAttribute("nullData", null);
